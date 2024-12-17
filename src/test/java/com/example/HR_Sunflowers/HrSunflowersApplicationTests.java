eec52a9de09b091b3c2b3925de175512351f2501
AsankaThennakoon <newsunflowers98@gmail.com>
2024-12-17 14:43:48 +0530

    initiate project

---
+++ src/test/java/com/example/HR_Sunflowers/HrSunflowersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HR_Sunflowers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrSunflowersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
